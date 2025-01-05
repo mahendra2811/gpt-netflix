32576c31b1691164026d3f3d828d8f342b1b90c8
Mahendra Singh Puniya <mahendrapuniya92@gmail.com>
2025-01-05 20:07:25 +0530

    error dcode

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Body from './components/Body';
 function App() {
   return (
     <div>
-        <h1>helllo this is app</h1>
-        <Header />
-        <Body />
-        
-      
+      <Header/>
+      <Body/>
+
+
     </div>
   );
 };
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Login";
+import Browse from "./Browse";
 
 const Body = () => {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
   return (
     <div>
-      <h1>Body</h1>
+      <RouterProvider router={appRouter} />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
